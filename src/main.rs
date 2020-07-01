4b17ca514eaa4eedf6974e1ed97c312187f29488
Preetha Selvam <preetha.selvam@jaydevops.com>
2020-07-02 05:29:56 +0530

    Sample program for structure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct Person {
+    name: String,
+    age: u32
+}
+
+impl Person {
+    pub fn new(name: String, age: u32) -> Self {
+        Person {
+            name,
+            age
+        }
+    }
+}
+
 fn main() {
-    let x = vec!["Hello", "World"];
-    let y: Vec<_> = x.iter().rev().collect();
-    println!("{:?}", y);
+    let mut people = vec![
+        Person::new("Zoe".to_string(), 25),
+        Person::new("Al".to_string(), 60),
+        Person::new("John".to_string(), 1),
+    ];
+
+    // Sort people by derived natural order (Name and age)
+    people.sort();
+
+    assert_eq!(
+        people,
+        vec![
+            Person::new("Al".to_string(), 60),
+            Person::new("John".to_string(), 1),
+            Person::new("Zoe".to_string(), 25),
+        ]);
+
+ 
+        people.reverse();
+        println!("{:#?}", people);
+
 }
